62f7b48d4ac7d03a04111014ac349525a83e43f5
mustafo898 <mustafoabdugafurov55@gmail.com>
2023-03-16 20:10:28 +0500

    second commit

--- app/src/main/java/com/dark_composer/simpletask/App.kt
+++ app/src/main/java/com/dark_composer/simpletask/App.kt
@@ -9,23 +9,10 @@ class App : Application() {
 
     companion object {
         lateinit var resource: Resources
-//        lateinit var sharedPref: EncryptedSharedPref
     }
 
     override fun onCreate() {
         super.onCreate()
         resource = resources
-
-//        sharedPref = EncryptedSharedPref(applicationContext)
-//        Log.d("OOOOOOOOOOOOOOOOOOOO", "onViewCreate: ${sharedPref.getNightMode()}")
-//
-//        if (sharedPref.getNightMode()) {
-//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-//        } else {
-//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-//        }
-
-//        sharedPref = EncryptedSharedPref(applicationContext)
-
     }
 }
